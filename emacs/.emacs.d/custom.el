e7ed6e8923433c57c413bf6e248c2c7be4e60d7a
Herdinger <herdinger@fastmail.fm>
2018-11-27 19:06:32 +0100

    Removed obsolete packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(org-M-RET-may-split-line (quote ((default))))
  '(package-selected-packages
    (quote
-    (ivy-hydra evil-magit magit elfeed-org esup org-bullets ranger counsel projectile oceanic-theme org-noter ace-window pdf-tools hydra general which-key evil-collection evil exec-path-from-shell use-package))))
+    (ivy-hydra evil-magit magit elfeed-org esup org-bullets ranger counsel oceanic-theme org-noter ace-window pdf-tools hydra general evil-collection evil exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
