6b689a8d5ff112214e675ffc96dbbef0db1d597f
Herdinger <herdinger@fastmail.fm>
2018-11-30 19:48:44 +0100

    Installed counsel-projectile

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(org-M-RET-may-split-line (quote ((default))))
  '(package-selected-packages
    (quote
-    (projectile which-key ivy-hydra evil-magit magit elfeed-org esup org-bullets ranger counsel oceanic-theme org-noter ace-window pdf-tools hydra general evil-collection evil exec-path-from-shell use-package))))
+    (counsel-projectile projectile which-key ivy-hydra evil-magit magit elfeed-org esup org-bullets ranger counsel oceanic-theme org-noter ace-window pdf-tools hydra general evil-collection evil exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
