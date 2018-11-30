488e5f3fd30c80e5328203360ec6474ec5ab70b7
Herdinger <herdinger@fastmail.fm>
2018-11-30 19:49:38 +0100

    Added shell to org-babel

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-M-RET-may-split-line (quote ((default))))
+ '(org-babel-load-languages (quote ((shell . t))))
  '(package-selected-packages
    (quote
     (counsel-projectile projectile which-key ivy-hydra evil-magit magit elfeed-org esup org-bullets ranger counsel oceanic-theme org-noter ace-window pdf-tools hydra general evil-collection evil exec-path-from-shell use-package))))
