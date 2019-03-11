e7a404b3f4bac0fbb64fd2c15f20dccd8733c329
Herdinger <herdinger@fastmail.fm>
2019-03-11 14:22:33 +0100

    Refined bootstrap.sh script a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! hash brew 2>/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew bundle
+
+stow -t "$HOME" bash emacs nvim
 
